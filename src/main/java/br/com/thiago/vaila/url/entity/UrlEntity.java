da11325c78d53876b79106e3747177aa2ce3e338
Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>
2025-09-19 19:29:20 -0300

    feature/url: Popula classe de dados da URL com parâmetros necessários.
    - Altera UrlEntity, adicionando parâmetros de id - utilizado para geração de uma URL automática, originalUrl para redirecionamento e customPath para criar uma URL customizada.
    
    Signed-off-by: Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>

--- src/main/java/br/com/thiago/vaila/url/entity/UrlEntity.java
+++ src/main/java/br/com/thiago/vaila/url/entity/UrlEntity.java
@@ -1,8 +1,30 @@
 package br.com.thiago.vaila.url.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
 
 @Entity
+@Table
+@Data
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(unique = true, nullable = false, length = 500)
+    private String originalUrl;
+
+    @Column(unique = true)
+    private String customPath;
 }
