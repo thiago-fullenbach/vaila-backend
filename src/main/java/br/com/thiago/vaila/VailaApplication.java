14c4f0b002eac34118edbb59fc6cb2956ca71f44
Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>
2025-09-19 18:26:42 -0300

    Commit inicial do projeto.
    Signed-off-by: Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>

---
+++ src/main/java/br/com/thiago/vaila/VailaApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.vaila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VailaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VailaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiago/vaila/VailaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.vaila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VailaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
